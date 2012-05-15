2ba5a035640ab1d1baa85b7aa702dd618d808de2
Jonas Wiklund <jonas.wiklund@atex.com>
2012-05-15 21:28:46 +0200

    writing writing writing

--- ws-plugin-model/src/main/java/com/atex/plugins/wspluginmodel/ModelWSWriter.java
+++ ws-plugin-model/src/main/java/com/atex/plugins/wspluginmodel/ModelWSWriter.java
@@ -6,6 +6,7 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.List;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -32,7 +33,65 @@ public class ModelWSWriter implements MessageBodyWriter<Model> {
         MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException
     {
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(entityStream));
-        pw.write(t.toString());
+        write(pw, t, 2);
         pw.flush();
+    }
+
+    private void write(PrintWriter pw, Model t, int depth)
+    {
+        if (depth <= 0) {
+            pw.print("{\"expanded\": \"false\"}");
+            return;
+        }
+        if (t == null) {
+            pw.print("null");
+        } else if (t.getModelType().isBasic()) {
+            pw.print('"');
+            pw.print(t.getModelType().exportToString(t));
+            pw.print('"');
+        } else if (t.getModelType().isArray()) {
+            if (t instanceof List<?>) {
+                List<?> l = (List<?>) t;
+                pw.print('[');
+                for (int i = 0 ; i < l.size() ; i++) {
+                    if (i != 0) {
+                        pw.print(", ");
+                    }
+                    Object o = l.get(i);
+                    if (o == null) {
+                        pw.print("null");
+                    } else if (o instanceof Model) {
+                        write(pw, (Model) o, depth - 1);
+                    } else if (t.getModelType().getArrayType() != null) {
+                        pw.write(t.getModelType().getArrayType().exportToString(o));
+                    }
+                }
+                pw.print(']');
+            } else {
+                pw.print(String.format("\"unknown list type '%s' with model '%s'\"", t.getModelType(), t.getClass()));
+            }
+        } else if (t.getModelType().isCompound()) {
+            pw.print('{');
+            boolean first = true;
+            for (String name : t.getAttributeNames()) {
+                if (first) {
+                    first = false;
+                } else {
+                    pw.print(", ");
+                }
+                pw.print(String.format("\"%s\": ", name));
+                Object o = t.getAttribute(name);
+                if (o == null) {
+                    pw.print("null");
+                } else if (o instanceof Model) {
+                    write(pw, (Model) o, depth - 1);
+                } else {
+                    pw.write(String.format("\"%s\"", t.getModelType().getAttributeType(name).exportToString(o)));
+                }
+            }
+            pw.print('}');
+        } else {
+            pw.print(String.format("\"unable to write json for model type '%s'\"", t.getModelType().getName()));
+        }
     }
 }
